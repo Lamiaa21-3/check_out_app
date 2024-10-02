9b63c257ec616f079a0237fa8842e4ca17370a4f
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-10-02 11:55:27 +0200

    check out repo implementation

---
+++ lib/features/check_out/data/repos/check_out_repo_impl.dart
@@ -0,0 +1,22 @@
+import 'package:check_out_app/core/utils/stripe_services.dart';
+import 'package:check_out_app/errors/failures.dart';
+import 'package:check_out_app/features/check_out/data/models/payment_intent_input_model.dart';
+import 'package:check_out_app/features/check_out/data/repos/check_out_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class CheckOutRepoImpl extends CheckOutRepo {
+  final StripeServices stripeServices = StripeServices();
+
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await stripeServices.makePayment(
+          paymentIntentInputModel: paymentIntentInputModel);
+      return right(null);
+    }
+    catch(e){
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+}
