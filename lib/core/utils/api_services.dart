bbc668e4a1b189c7c222560a27348b6fe81443b3
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-10-02 09:34:02 +0200

    add  api services

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final Dio dio = Dio();
+
+  Future<Response> post(
+      {required body,
+      required String url,
+      required String token,
+      required String? contentType}) {
+    final response=  dio.post(url,data: body,options:Options (
+    contentType : contentType,
+      headers: {
+         'Authorization': 'Bearer$token'
+      }
+    ));
+    return response;
+  }
+}
